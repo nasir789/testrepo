3c8c9fdeacfae22e1af25c1143cd07ee5c8df639
Araib Nasir <araibn@gmail.com>
2015-12-17 00:09:06 -0500

    implemented ngHide functionality to words on front end, connecting with function in bankController, moved function from inputController to bankCont, finished populating db, looped through check function

--- word_game/public/js/bankController.js
+++ word_game/public/js/bankController.js
@@ -4,19 +4,43 @@ BankController.$inject = ['$http'];
 
 function BankController($http){
   var self = this;
-  self.test = "this is a test" //
+  self.test = "The Agglutinative Game" //
   self.all = [];
   self.taco="";
   // self.wordRender=['this']//
   getWords();
+
   function getWords(){
     $http
       .get('http://localhost:8000/return')
       .then(function(response){
         console.log(response.data.name);
+
         self.all = response.data.name;
+
+        self.all.forEach(function(word){
+          word.guessed = false;
+        });
+        console.log(self.all)
       });
 
-  }
+  };
 
+  this.isCorrect = function(word) {
+    // // var element = Document.get the word the users trying to matc
+
+
+      for (i = 0; i < self.all.length; i++) {
+        var check = self.all[i].name;
+        if ( word == check ) {
+          self.all[i].guessed = true;
+          console.log('meow')
+        }
+      };
+
+    if ( word !== check ) {
+      // document.getElementsByClassName("checkWord").style.color = 'red';
+      console.log('woof')
+      }
+  }
 }
--- word_game/public/js/inputController.js
+++ word_game/public/js/inputController.js
@@ -1,53 +1,39 @@
-
-angular.module('wordsGame').controller('InputController', InputController);
-
-
-  function InputController() {
-
-    this.isCorrect = function(word) {
-      // // var element = Document.get the word the users trying to match
-      var check = document.getElementsByClassName("checkWord")[1].innerText;
-
-      if ( word == check ) {
-        $("#demo").text(function(){
-          (".checkWord").hide();
-        });
-        // document.getElementsByClassName("checkWord").style.color = 'green';
-        console.log('meow')
-      };
-      if ( word !== check ) {
-        // document.getElementsByClassName("checkWord").style.color = 'red';
-        console.log('woof')
-      };
-
-
-      // do {
-      //   $("#hide").click(function(){
-      //  $("p").hide();
-      // });
-      // } while ();
-
-
-
-      // THIS IS ALL JUST TRUE FALSE
-      //1.
-      //  word == word true? then show the next word
-      // first just match words, if word matches turn word green if word doesnt match turn it red
-      // if word matches move to next word
-      // if the user deletes the non-matching character word goes back to black
-      // 2.
-      // match letters
-      // then when the user types one letter it matches against the first letter of the word
-      // if it matches word stays black
-      // if it doesnt whole word turns red
-      // same win state lose state as 1
-      // 3.
-      // if a letter matches turn the indiviudal letter green
-      // if a letter doesnt match turn the whole word word
-      // 4.
-      // if a letter doesnt match just turn individual letter red
-
-      // console.log(word);
-
-    }
-  };
+// 
+// angular.module('wordsGame').controller('InputController', InputController);
+//
+//
+//   function InputController() {
+//
+//
+//
+//
+//       // do {
+//       //   $("#hide").click(function(){
+//       //  $("p").hide();
+//       // });
+//       // } while ();
+//
+//
+//
+//       // THIS IS ALL JUST TRUE FALSE
+//       //1.
+//       //  word == word true? then show the next word
+//       // first just match words, if word matches turn word green if word doesnt match turn it red
+//       // if word matches move to next word
+//       // if the user deletes the non-matching character word goes back to black
+//       // 2.
+//       // match letters
+//       // then when the user types one letter it matches against the first letter of the word
+//       // if it matches word stays black
+//       // if it doesnt whole word turns red
+//       // same win state lose state as 1
+//       // 3.
+//       // if a letter matches turn the indiviudal letter green
+//       // if a letter doesnt match turn the whole word word
+//       // 4.
+//       // if a letter doesnt match just turn individual letter red
+//
+//       // console.log(word);
+//
+//     }
+//   };
